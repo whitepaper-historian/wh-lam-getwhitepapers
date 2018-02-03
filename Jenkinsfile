da0d916141006d7decf9b3536af54f0dbdcb7387
gcardonag <gcardonagarcia@gmail.com>
2018-02-02 20:11:37 -0500

    Adding index.js and dummy Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,20 @@
+//This pipeline assumes the following plugins exist on the Jenkins instance:
+//JSLint
+
+pipeline {
+	agent any
+
+	stages {
+		stage('Build') {
+			//Nothing here at the moment
+		}
+
+		stage('Test') {
+			//Nothing here either. I should set a linter here.
+		}
+
+		stage('Deploy') {
+			//Deploy steps here
+		}
+	}
+}
---
+++ lambda/index.js
@@ -0,0 +1,40 @@
+const AWS = require('aws-sdk');
+
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = function(event, context, callback){
+
+    var params = {
+        TableName: "whitepapers",
+        Select: "SPECIFIC_ATTRIBUTES",
+        ProjectionExpression: "title, updated, pdf_link, kindle_link"
+    };
+    return ddb.scan(params, function(err, data) {
+        if(err) {
+            errorResponse(err, context.awsRequestId, callback);
+            return;
+        }
+
+        callback(null, {
+            statusCode: 201,
+            body: JSON.stringify(data),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+    });
+
+};
+
+function errorResponse(errorMessage, awsRequestId, callback) {
+  callback(null, {
+    statusCode: 500,
+    body: JSON.stringify({
+      Error: errorMessage,
+      Reference: awsRequestId,
+    }),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
+}
